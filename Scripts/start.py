a55fc4b77fafef72fa393fae63007e3d143cdcac
ELENAZAZA <m.zaza16@studenti.uniba.it>
2023-12-13 20:38:47 +0100

    Implemented event recognition

--- Scripts/start.py
+++ Scripts/start.py
@@ -5,30 +5,45 @@ import os
 def Ner(input_file, output_file):
     nlp = spacy.load("it_core_news_sm")
     
-    # Leggi il testo dal file
     with open(input_file, 'r', encoding='utf-8') as file:
         testo = file.read()
         
     doc = nlp(testo)
     
-    # Prepara i risultati per il salvataggio in JSON
     risultati = []
     for token in doc:
-        if token.ent_type_ != "":
+        if token.ent_type_ != "" and token.ent_type_ != "MISC" :
             risultati.append({
                 "testo": token.text,
                 "entita": token.ent_type_
             })
         
-    # Salva i risultati in un file JSON
+    with open(output_file, 'w', encoding='utf-8') as json_file:
+        json.dump(risultati, json_file, ensure_ascii=False, indent=2)
+
+def Event(input_file, output_file):
+    nlp = spacy.load("Small_Model_Efficiency\output\model-best")
+    
+    with open(input_file, 'r', encoding='utf-8') as file:
+        testo = file.read()
+
+    doc = nlp(testo)
+    risultati=[]
+    for token in doc:
+        if token.ent_type_ == "EVENT":
+                risultati.append({
+                    "testo": token.text,
+                    "entita": token.ent_type_
+                })
+        
     with open(output_file, 'w', encoding='utf-8') as json_file:
         json.dump(risultati, json_file, ensure_ascii=False, indent=2)
 
 print(os.getcwd())
 
-# Specifica i percorsi del file di input e del file di output JSON
 input_file_path = "data/input/10502_due_sposi_in_viaggio_brat.txt"
 output_file_path = "data/output/output.json"
+output_file_path1 = "data/output/output_event.json"
 
-# Esegui la funzione con i percorsi specificati
 Ner(input_file_path, output_file_path)
+Event(input_file_path,output_file_path1)
